9281157e82f3099113050e1122f918f03d56103e
mahakdixit12 <dixitmahak63@gmail.com>
2024-12-18 22:19:43 +0530

    ass 9

---
+++ assignment 9/q8.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+int main() {
+int n;
+cin >> n;
+for(int i = 0; i < n; ++i) {
+cout << " ";
+}
+cout << 1 << endl;
+for(int i = 1; i <= n; ++i) {
+for(int j = 0; j < n - i; j++) {
+cout << " ";
+}
+cout << i;
+for(int j = 0; j < 2 * i - 1; ++j) {
+cout << " ";
+}
+cout << i << endl;
+}
+return 0;
+}
