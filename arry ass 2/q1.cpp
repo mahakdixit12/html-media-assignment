1684a757a306733f248dfe333688068d99e91dc0
mahakdixit12 <dixitmahak63@gmail.com>
2025-01-26 20:08:26 +0530

    array2

---
+++ arry ass 2/q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int x;
+cin>>x;
+int a[5];
+cout<<”Enter 5 elements for the array”<<endl;
+for(int i=0;i<5;i++)cin>>a[i];
+int ans=0;
+for(int i=0;i<5;i++){
+if(a[i]>x) ans++;
+}
+cout<<ans<<endl;
+return 0;
+}
