181795158ad81e3c84df175c3467cfbde0676976
mahakdixit12 <dixitmahak63@gmail.com>
2025-01-28 12:02:40 +0530

    mod 1

---
+++ mod 1/q2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int n;
+cin >> n;
+string s;
+cin >> s;
+int c = 0;
+for (int i = 0; i < (int)s.size(); i ++) {
+if (s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'u' || s[i] == 'i')
+{
+c++;
+}
+}
+cout << n - c;
+}
---
+++ mod 1/q3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check(string &s) {
+int i = 0, j = (int)s.size() - 1;
+while (i <= j) {
+if (s[i] != s[j]) return false;
+i++, j--;
+}
+}
+int main() {
+int n;
+cin >> n;
+string s;
+cin >> s;
+cout << (check(s) ? "YES" : "NO");
+}
