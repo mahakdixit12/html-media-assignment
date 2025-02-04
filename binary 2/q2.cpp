6b534fe998fda005116acd7c9e4b902e4255a973
mahakdixit12 <dixitmahak63@gmail.com>
2025-02-04 11:07:48 +0530

    binary 2

---
+++ binary 2/q2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a[] = {1,2,4,7,10,12,15,18};
+int lo = 0 , hi = 1;
+int tgt = 10;
+while(a[hi] < tgt){
+lo = hi;
+hi *= 2;
+}
+cout<<lo<<" "<<hi<<endl;
+bool flag = false;
+while(lo <= hi){
+int mid = lo + (hi - lo)/2;
+if(a[mid] ==tgt){
+flag = true;
+break;
+}
+else if(a[mid] > tgt){
+hi = mid - 1;
+}
---
+++ binary 2/q3.cpp
@@ -0,0 +1,12 @@
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+int m = matrix.size();
+int n = matrix[0].size();
+int lo = 0 , hi = n*m - 1;
+while(lo <= hi){
+int mid = lo + (hi - lo)/2;
+if(matrix[mid/n][mid%n] == target)return true;
+else if(matrix[mid/n][mid%n] > target)hi = mid - 1;
+else lo = mid + 1;
+}
+return false;
+}
